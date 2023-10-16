753c5a5ad719ff8a7714f05fa70df1ce8d7470ee
Repositoir <86570439+Repositoir@users.noreply.github.com>
2023-10-17 01:33:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game('Banana Shooter'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('Game'))
     print("Client is ready and running!")
 
 
